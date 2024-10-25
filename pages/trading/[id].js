bfc6f335e8a0476ba0fe57a1ae403df5105ff43a
aeschalna <aeschalna@gmail.com>
2024-10-25 10:50:28 -0600

    added internal size to product preview, descriptions complete

--- pages/trading/[id].js
+++ pages/trading/[id].js
@@ -69,7 +69,8 @@ export default function ProductPreview() {
             <h1 className="mt-4 text-4xl font-bold text-gray-900">
               {product.name}
             </h1>
-            <p className="mt-4 text-xl text-gray-500">${product.price}</p>
+            <p className="mt-4 text-2xl text-gray-500">${product.price}</p>
+            <p className="mt-4 text-md text-gray-700">{product.internal_size}</p>
             {/* Color Options */}
             <div className="mt-6">
               <h3 className="text-sm mb-2 font-medium text-gray-700">

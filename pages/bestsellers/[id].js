0385457aac6c75c022758b7750d27b9ed2bd551c
aeschalna <aeschalna@gmail.com>
2024-10-25 10:54:22 -0600

    typo fixed

--- pages/bestsellers/[id].js
+++ pages/bestsellers/[id].js
@@ -76,7 +76,7 @@ export default function ProductPreview() {
             <p className="mt-4 text-md text-gray-700">{product.internal_size}</p>
             {/* Color Options */}
             <div className="mt-6">
-              <h3 className="text-sm mb-2 font-medium text-gray-700">Color Options</h3>
+              <h3 className="text-sm mb-2 font-medium text-gray-700">Color</h3>
               <RadioGroup
                 value={selectedColor}
                 onChange={setSelectedColor}
--- pages/trading/[id].js
+++ pages/trading/[id].js
@@ -74,7 +74,7 @@ export default function ProductPreview() {
             {/* Color Options */}
             <div className="mt-6">
               <h3 className="text-sm mb-2 font-medium text-gray-700">
-                Color Options
+                Color
               </h3>
               <RadioGroup
                 value={selectedColor}

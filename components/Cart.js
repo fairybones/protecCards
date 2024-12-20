6194a18689572780f2643820837d86abcdd0cf28
aeschalna <aeschalna@gmail.com>
2024-12-19 19:20:41 -0700

    debugging netlify

--- components/Cart.js
+++ components/Cart.js
@@ -1,11 +1,21 @@
 "use client";
 
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { CartContext } from "context/CartContext";
 import Link from "next/link";
 
 const Cart = () => {
   const { addItemToCart, deleteItemFromCart, cart } = useContext(CartContext);
+  const [customerCart, setCustomerCart] = useState(null);
+
+  // ensure cart is only used after hydration
+  useEffect(() => {
+    setCustomerCart(cart);
+  }, [cart]);
+
+  if (!customerCart) {
+    return <p>Loading...</p>
+  }
 
   const increaseQty = (cartItem) => {
     const newQty = cartItem?.quantity + 1;
@@ -21,29 +31,29 @@ const Cart = () => {
     addItemToCart(item);
     }
   };
-  const subtotal = cart?.cartItems?.reduce(
+  const subtotal = customerCart?.cartItems?.reduce(
     (acc, item) => acc + item.quantity * item.price,
     0
   );
   const taxAmount = (subtotal * 0.15).toFixed(2);
   const total = (Number(subtotal) + Number(taxAmount)).toFixed(2);
-
+  
   return (
     <div>
       <section className="py-5 sm:py-7">
         <div className="container max-w-screen-xl mx-auto px-4">
           <h2 className="text-3xl font-semiold mb-2">
-            {cart?.cartItems.length || 0} Items in Cart
+            {customerCart?.cartItems.length || 0} Items in Cart
           </h2>
         </div>
       </section>
-      {cart?.cartItems?.length > 0 && (
+      {customerCart?.cartItems?.length > 0 && (
         <section className="py-10">
           <div className="container max-w-screen-xl mx-auto px-4">
             <div className="flex flex-col md:flex-row gap-4">
               <main className="md:w-3/4">
                 <article className="border border-gray-200 bg-white shadow-sm rounded mb-5 p-3 lg:p-5">
-                  {cart?.cartItems?.map((cartItem) => (
+                  {customerCart?.cartItems?.map((cartItem) => (
                     <div key={cartItem.product}>
                       <div className="flex flex-wrap lg:flex-row gap-5 mb-4">
                         <div className="w-full lg:w-2/5 xl:w-2/4">
@@ -55,7 +65,7 @@ const Cart = () => {
                             </div>
                             <figcaption className="ml-3">
                               <p>
-                                <a href="#" className="hover:text-emerald-600">
+                                <a href={`/${cartItem.product}`} className="hover:text-emerald-600">
                                   {cartItem.name}
                                 </a>
                               </p>
@@ -119,7 +129,7 @@ const Cart = () => {
                     <li className="flex justify-between text-gray-600 mb-1">
                       <span>Total Units:</span>
                       <span className="text-emerald-700">
-                        {cart?.cartItems?.reduce(
+                        {customerCart?.cartItems?.reduce(
                             (acc, item) => acc + item.quantity,
                             0
                         )}
@@ -135,18 +145,23 @@ const Cart = () => {
                       <span className="text-xs text-gray-700 mb-1">*Shipping calculated at checkout.</span>
                     </li>
                   </ul>
-                  {/* {cart?.cartItems?.length === 0 && (
+                  {cart?.cartItems?.length === 0 && (
                     <div className="text-center py-10">
                       <h2 className="text-2xl font-semibold mb-4">Your cart is empty</h2>
                       <Link href="/" className="text-emerald-700 hover:underline">Continue Shopping</Link>
                     </div>
-                  )} */}
-                  <Link href="/" className="text-emerald-700 hover:underline">Continue Shopping</Link>
+                  )}
                 </article>
               </aside>
             </div>
           </div>
         </section>
+      )}
+      {customerCart?.cartItems?.length === 0 && (
+        <div className="text-center py-10">
+          <h2 className="text-2xl font-semibold mb-4">Your cart is empty.</h2>
+          <Link href="/" className="text-emerald-700 hover:underline">Continue Shopping</Link>
+          </div>
       )}
     </div>
   );
--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,5 +1,7 @@
-import Cart from "components/Cart";
 import React from "react";
+import dynamic from "next/dynamic";
+
+const Cart = dynamic(() => import("components/Cart"), { ssr: false });
 
 const CartPage = () => {
     return <Cart />;

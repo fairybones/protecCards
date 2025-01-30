7880ff83e2d97094dbe685420bd8820d8a6fb8d7
aeschalna <aeschalna@gmail.com>
2025-01-30 15:34:39 -0700

    POST 200

--- components/Cart.js
+++ components/Cart.js
@@ -1,8 +1,27 @@
-"use client";
-
 import React, { useContext, useEffect, useState } from "react";
 import { CartContext } from "context/CartContext";
 import Link from "next/link";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+);
+const handleCheckout = async (cart) => {
+  const res = await fetch("/api/checkout_sessions", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ cartItems: cart }),
+  });
+  if (!res.ok) {
+    const errorData = await res.json();
+    console.error("Checkout session failed:", errorData);
+    return;
+  }
+
+  const { sessionId } = await res.json();
+  const stripe = await stripePromise;
+  stripe.redirectToCheckout({ sessionId });
+};
 
 const Cart = () => {
   const { addItemToCart, deleteItemFromCart, cart } = useContext(CartContext);
@@ -14,7 +33,7 @@ const Cart = () => {
   }, [cart]);
 
   if (!customerCart) {
-    return <p>Loading...</p>
+    return <p>Loading...</p>;
   }
 
   const increaseQty = (cartItem) => {
@@ -26,34 +45,38 @@ const Cart = () => {
   const decreaseQty = (cartItem) => {
     if (cartItem?.quantity > 1) {
       const newQty = cartItem?.quantity - 1;
-    const item = { ...cartItem, quantity: newQty };
+      const item = { ...cartItem, quantity: newQty };
 
-    addItemToCart(item);
+      addItemToCart(item);
+    } else {
+      deleteItemFromCart(cartItem.product);
     }
   };
-  const findSubtotal = (cartItems) => { 
+  const findSubtotal = (cartItems) => {
     if (!cartItems || cartItems.length === 0) return 0;
-  return cartItems.reduce((acc, item) => acc +item.quantity * item.price, 0).toFixed(2);
+    return cartItems
+      .reduce((acc, item) => acc + item.quantity * item.price, 0)
+      .toFixed(2);
   };
   const subtotal = findSubtotal(customerCart?.cartItems);
 
   const taxAmount = (subtotal * 0.15).toFixed(2);
   const total = (Number(subtotal) + Number(taxAmount)).toFixed(2);
-  
+
   return (
     <div>
       <section className="py-5 sm:py-7">
-          <h2 className="text-xl font-semiold mb-2">
-            {customerCart?.cartItems.length || 0} Items in Cart!
-          </h2>
+        <h2 className="text-xl font-semiold mb-2">
+          {customerCart?.cartItems.length || 0} Items in Cart!
+        </h2>
       </section>
       <img
-          alt={"ProTec Logo"}
-          src={
-            "https://lqgkaiftunbbvlkylfga.supabase.co/storage/v1/object/public/product-photos/hero.png"
-          }
-          className="w-full max-w-xs sm:max-w-md md:max-w-lg lg:max-w-screen-lg"
-        />
+        alt={"ProTec Logo"}
+        src={
+          "https://lqgkaiftunbbvlkylfga.supabase.co/storage/v1/object/public/product-photos/hero.png"
+        }
+        className="w-full max-w-xs sm:max-w-md md:max-w-lg lg:max-w-screen-lg"
+      />
       {customerCart?.cartItems?.length > 0 && (
         <section className="py-6">
           <div className="container mx-auto px-4 max-w-screen-xl">
@@ -61,7 +84,10 @@ const Cart = () => {
               <main className="md:w-3/4 h-full">
                 <article className="border border-gray-200 bg-white shadow-sm rounded-lg mb-5p-5 max-h-full">
                   {customerCart?.cartItems?.map((cartItem) => (
-                    <div key={cartItem.product} className="flex flex-wrap mt-5 ml-5 items-center gap-6 border-b pb-6">
+                    <div
+                      key={cartItem.product}
+                      className="flex flex-wrap mt-5 ml-5 items-center gap-6 border-b pb-6"
+                    >
                       <div className="flex flex-wrap lg:flex-row gap-5 mb-6">
                         <div className="w-full lg:w-1/4">
                           <figure className="flex leading-5">
@@ -72,11 +98,16 @@ const Cart = () => {
                             </div>
                             <figcaption className="flex-1 min-w-0 ml-20">
                               <p className="text-lg font-medium text-gray-800">
-                                <Link href={`/${cartItem.product}`} className="hover:text-emerald-600">
+                                <Link
+                                  href={`/${cartItem.product}`}
+                                  className="hover:text-emerald-600"
+                                >
                                   {cartItem.name}
                                 </Link>
                               </p>
-                              <p className="text-gray-600 mt-3">Price: ${cartItem.price.toFixed(2)}</p>
+                              <p className="text-gray-600 mt-3">
+                                Price: ${cartItem.price.toFixed(2)}
+                              </p>
                             </figcaption>
                           </figure>
                         </div>
@@ -129,7 +160,9 @@ const Cart = () => {
               </main>
               <aside className="md:w-1/4 h-full">
                 <article className="border border-gray-200 bg-white shadow-sm rounded-lg mb-5 p-5">
-                <h3 className="text-lg font-semibold text-gray-800 mb-4">Order Summary</h3>
+                  <h3 className="text-lg font-semibold text-gray-800 mb-4">
+                    Order Summary
+                  </h3>
                   <ul className="text-gray-700 space-y-2">
                     <li className="flex justify-between mb-1">
                       <span>Subtotal:</span>
@@ -139,8 +172,8 @@ const Cart = () => {
                       <span>Total Units:</span>
                       <span className="text-emerald-700">
                         {customerCart?.cartItems?.reduce(
-                            (acc, item) => acc + item.quantity,
-                            0
+                          (acc, item) => acc + item.quantity,
+                          0
                         )}
                       </span>
                     </li>
@@ -153,14 +186,27 @@ const Cart = () => {
                       <span>${total}</span>
                     </li>
                   </ul>
-                  <p className="text-xs text-gray-700 mt-2 mb-1">*Shipping calculated at checkout.</p>
-                  <button className="w-full mt-4 bg-emerald-600 text-white py-2 rounded-lg hover:bg-emerald-700 transition">
-            Proceed to Checkout
-          </button>
+                  <p className="text-xs text-gray-700 mt-2 mb-1">
+                    *Shipping calculated at checkout.
+                  </p>
+                  <button
+                    className="w-full mt-4 bg-emerald-600 text-white py-2 rounded-lg hover:bg-emerald-700 transition"
+                    onClick={() => handleCheckout(customerCart?.cartItems)}
+                    disabled={customerCart?.cartItems?.length === 0}
+                  >
+                    Proceed to Checkout
+                  </button>
                   {cart?.cartItems?.length === 0 && (
                     <div className="text-center py-10">
-                      <h2 className="text-2xl font-semibold mb-4">Your cart is empty.</h2>
-                      <Link href="https://protecaccessories.com/" className="text-emerald-700 hover:underline">Continue Shopping</Link>
+                      <h2 className="text-2xl font-semibold mb-4">
+                        Your cart is empty.
+                      </h2>
+                      <Link
+                        href="https://protecaccessories.com/"
+                        className="text-emerald-700 hover:underline"
+                      >
+                        Continue Shopping
+                      </Link>
                     </div>
                   )}
                 </article>
@@ -172,8 +218,10 @@ const Cart = () => {
       {customerCart?.cartItems?.length === 0 && (
         <div className="text-center py-10">
           <h2 className="text-2xl font-semibold mb-4">Your cart is empty.</h2>
-          <Link href="/" className="text-emerald-700 hover:underline">Continue Shopping</Link>
-          </div>
+          <Link href="/" className="text-emerald-700 hover:underline">
+            Continue Shopping
+          </Link>
+        </div>
       )}
     </div>
   );
--- pages/api/checkout_sessions.js
+++ pages/api/checkout_sessions.js
@@ -1,26 +1,45 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2024-12-18.acacia",
+});
 
 export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        try {
-            const session = await stripe.checkout.sessions.create({
-                line_items: [
-                    {
-                        price: '{{PRICE_ID}}',
-                        quantity: 1,
-                    },
-                ],
-                mode: 'payment',
-                success_url: `${req.headers.origin}/?success=true`,
-                cancel_url: `${req.headers.origin}/?canceled=true`,
-                automatic_tax: {enabled: true},
-            });
-            res.redirect(303, session.url);
-        } catch (err) {
-            res.status(err.statusCode || 500).json(err.message);
-        }
-    } else {
-        res.setHeader('Allow', 'POST');
-        res.status(405).end('Method Not Allowed');
+  if (req.method === "POST") {
+    try {
+      const { cartItems } = req.body;
+      console.log(cartItems);
+
+      const line_items = cartItems.map((item) => ({
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: item.name,
+          },
+          unit_amount: Math.round(item.price * 100),
+        },
+        quantity: item.quantity,
+      }));
+
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items,
+        mode: "payment",
+        success_url: `${req.headers.origin}/success?session_id={CHECKOUT_SESSION_ID}`,
+        // success_url: `${process.env.NEXT_PUBLIC_SITE_URL}/success`,
+        cancel_url: `${process.env.NEXT_PUBLIC_SITE_URL}/cart`,
+        // cancel_url: `${req.headers.origin}/?canceled=true`,
+        automatic_tax: { enabled: true },
+      });
+      res.status(200).json({ sessionId: session.id });
+      // res.redirect(303, session.url);
+    } catch (err) {
+        console.error("Stripe Checkout error:", err);
+      res.status(err.statusCode || 500).json(err.message);
     }
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end("Method Not Allowed");
+    console.log("request recieved!", req.body);
+  }
 }
